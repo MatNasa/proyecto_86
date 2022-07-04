7eda6729c6343d2170f4d0d8789c7a2c39df4276
MatNasa <101373206+MatNasa@users.noreply.github.com>
2022-07-04 18:10:33 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+	fabric.Image.fromURL('BirthdayImage.jpg', function(Img){
+	block_image_objet = Img;
+
+    block_image_objet
+    block_image_objet
+    block_image_objet.set({top:0,left:0})
+    canvas.add(block_image_objet);
+    });
+}
+
+function playSound(){
+	x.play();
+}
